9791de702718dbdf6c0790b62c87c3734481cf3b
Prakash Chokalingam <prakash.chokalingam@freshworks.com>
2019-11-23 11:47:32 +0530

    [CHORE] functions code

--- functions/bot.js
+++ functions/bot.js
@@ -1,4 +1,8 @@
+// https://chitti-the-bot.netlify.com/.netlify/functions/bot
+
 exports.handler = function(event, context, callback) {
+  console.log(event)
+  console.log(context)
   callback(null, {
     statusCode: 200,
     body: "Hello, World"
