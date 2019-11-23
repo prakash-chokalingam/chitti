44bd1286766b56033e33bf96e148be2f46a37abf
Prakash Chokalingam <prakash.chokalingam@freshworks.com>
2019-11-23 11:51:23 +0530

    Seprator

--- functions/bot.js
+++ functions/bot.js
@@ -1,8 +1,10 @@
 // https://chitti-the-bot.netlify.com/.netlify/functions/bot
 
 exports.handler = function(event, context, callback) {
+  console.log('#################')
   console.log(event)
   console.log(context)
+  console.log('#################')
   callback(null, {
     statusCode: 200,
     body: "Hello, World"
