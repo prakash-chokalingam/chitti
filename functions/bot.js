2027a083b6f31d9f783f893084d21ce218c19dbe
Prakash Chokalingam <prakash.chokalingam@freshworks.com>
2019-11-23 21:50:07 +0530

    [test] cloud prod test

--- functions/bot.js
+++ functions/bot.js
@@ -1,23 +1,46 @@
 // https://chitti-the-bot.netlify.com/.netlify/functions/bot
 
-const validateHeader = 'Google-Dynamite';
+const validHeader = 'Google-Dynamite';
 
 exports.handler = function(event, context, callback) {
-  let { headers } = event;
-  console.log('teterrr')
-  console.log('****', headers['user-agent']);
-  if (headers['user-agent'] === validateHeader) { // via Google chat ?
-    console.log('came')
-    callback(null, {
-      statusCode: 200,
-      body: {
-        text: 'Thanks for adding me'
-      }
-    });
-  } else {
+  let { headers, body } = event;
+
+  // internal methods
+  let replayback = (status, msg) => {
     callback(null, {
       statusCode: 500,
       body: "Invalid user agent"
     });
+  };
+
+  let addedToSpace = () => {
+    console.log('Added to the space');
+    console.log('x')
+    replayback(200, {
+      "text": "Your pizza delivery *has arrived*!\nThank you for using _Pizza Bot!_"
+    });
+  };
+
+
+
+  // lets execute
+  if (headers['user-agent'] === validHeader) { // via Google chat ?
+    switch (body.type) {
+      case 'ADDED_TO_SPACE':
+        console.log('Added to the space');
+        addedToSpace();
+        break;
+
+      default:
+        break;
+    }
+    // callback(null, {
+    //   statusCode: 200,
+    //   body: {
+    //     text: 'Thanks for adding me'
+    //   }
+    // });
+  } else {
+    replayback(500, "Invalid user agent");
   }
 }
