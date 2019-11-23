f24b42cb76c8c160036a648292a7d8aec2083808
Prakash Chokalingam <prakash.chokalingam@freshworks.com>
2019-11-23 21:04:12 +0530

    test

--- functions/bot.js
+++ functions/bot.js
@@ -1,12 +1,24 @@
 // https://chitti-the-bot.netlify.com/.netlify/functions/bot
 
+const validateHeader = 'Google-Dynamite';
+
 exports.handler = function(event, context, callback) {
+  let { headers } = event;
+  if (headers['user-agent'] === validateHeader) {
+    callback(null, {
+      statusCode: 200,
+      body: {
+        text: 'Thanks for adding me'
+      }
+    });
+  } else {
+    callback(null, {
+      statusCode: 500,
+      body: "Invalid user agent"
+    });
+  }
   console.log('#################')
   console.log(event)
   console.log(context)
   console.log('#################')
-  callback(null, {
-    statusCode: 200,
-    body: "Hello, World"
-  });
 }
