204c41d5510ed9d53a24c7d269e4d6343bf5e133
aravindhan-pugazhendhi <aravindhan.pugazhendhi@freshworks.com>
2020-01-16 14:54:34 +0530

    Add pending tickets and due period

--- functions/integrations/freshdesk.js
+++ functions/integrations/freshdesk.js
@@ -1,21 +1,16 @@
 const fetch = require("node-fetch").default; // https://github.com/bitinn/node-fetch/issues/450
+const moment = require('moment');
 
 class Freshdesk {
   async checkOpenL2Tickets (config, callback) {
     let { domain, group, key } = config.freshdesk;
 
     let buff = new Buffer.from(`${key}:X`).toString('base64');
-    let url = `https://${domain}.freshdesk.com/api/v2/search/tickets?query="group_id:${group} AND status:2"`;
-    let tickets = await fetch(url, {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Basic ${buff}`
-        }
-      }).then(res => res.json());
 
-    let results = tickets.results || [];
+    let openTicketResult = await this.fetchTickets(domain, group, 2, buff)
+    let pendingTicketResult = await this.fetchTickets(domain, group, 3, buff)
     let response = { cards: [] }
-    let total = results.length;
+    let total = openTicketResult.length + pendingTicketResult.length;
 
     if (!total) {
       response.text = '<users/all> Hurray! great work folks 🎉'
@@ -28,25 +23,77 @@ class Freshdesk {
           {
             keyValue: {
               topLabel: '',
-              content: `L2 tickets (${total})`,
+              content: `L2 Open Tickets (${openTicketResult.length})`,
               icon: 'TICKET'
             }
           },
         ]
       }]
     });
+    response.cards.push({
+      sections: [{
+        widgets: [
+          {
+            keyValue: {
+              topLabel: '',
+              content: `L2 Pending Tickets (${pendingTicketResult.length})`,
+              icon: 'TICKET'
+            }
+          },
+        ]
+      }],
+    });
 
 
-    results.forEach(ticket => {
-      let { id, subject, is_escalated } = ticket;
+    this.appendTicketsToCard(response, openTicketResult, domain, 0)
+    this.appendTicketsToCard(response, pendingTicketResult, domain, 1)
+
+    let results = openTicketResult.concat(pendingTicketResult);
+    // consolidated
+    let overdued = results.filter(ticket => ticket.is_escalated).length;
+    if (overdued) {
+      response.cards.push({
+        sections: [{
+          widgets: [{
+              textParagraph: {
+              text: `<font color=\"#ff0000\">Total overdue tickets: <b>${overdued}</b></font>`
+            }
+          }]
+        }]
+      });
+    }
+
+    callback(response)
+
+  }
+
+  async fetchTickets(domain, group, status, buff) {
+    let url = `https://${domain}.freshdesk.com/api/v2/search/tickets?query="group_id:${group} AND status:${status}"`;
+    let tickets = await fetch(url, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Basic ${buff}`
+      }
+    }).then(res => res.json());
+    let ticketResults = tickets.results || [];
+    return ticketResults;
+  };
+
+  appendTicketsToCard(response, result, domain, index) {
+    result.forEach(ticket => {
+      let { id, subject, due_by } = ticket;
       let url =  `${domain}.freshdesk.com/a/tickets/${id}`;
       subject = (subject.length > 60) ? `${subject.substring(0, 60)}...` : subject;
-      let isOverdue = (is_escalated) ? '<font color=\"#ff0000\"><b>(overdue)</b></font>': '';
 
-      response.cards[0].sections.push({
+      let isEscalated = moment().diff(moment(due_by)) > 0
+      let isOverdue = (isEscalated) ? '<font color=\"#ff0000\"><b>(overdue)</b></font>': '';
+      let dueBy = moment(due_by).fromNow(true);
+      let deadLineText = (moment().diff(moment(due_by)) > 0) ? `<b>Overdue by: ${dueBy}</b>` : `<b>Due in: ${dueBy}</b>`
+
+      response.cards[index].sections.push({
         widgets: [{
           textParagraph: {
-            text: `${isOverdue} <b>#${id}</b> - ${subject}`
+            text: `${isOverdue} <b>#${id}</b> - ${subject} \n${deadLineText}`
           },
           buttons: [
             {
@@ -63,22 +110,7 @@ class Freshdesk {
         }]
       })
     });
-
-
-    // consolidated
-    let overdued = results.filter(ticket => ticket.is_escalated).length;
-    if (overdued) {
-      response.cards[0].sections.push({
-        widgets: [{
-            textParagraph: {
-            text: `<font color=\"#ff0000\">Total overdue tickets: <b>${overdued}</b></font>`
-          }
-        }]
-      });
-    }
-
-    callback(response)
-  }
-};
+  };
+}
 
 module.exports = Freshdesk;
